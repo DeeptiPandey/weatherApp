88d3442bee3e61288e5bcb9fed028f809dadfb8a
DeeptiPandey <deepti1932010@akgec.ac.in>
2023-09-02 01:09:53 +0530

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b91714bd41dc6c0a583855521cd7b737",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
